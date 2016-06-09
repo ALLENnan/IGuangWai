a60a243f46e7828d5c706e8d42deeee2295eba72
Allen <742890434@qq.com>
2016-06-10 00:53:47 +0800

    init

--- src/com/allen/iguangwai/AppConfig.java
+++ src/com/allen/iguangwai/AppConfig.java
@@ -1,12 +1,12 @@
 package com.allen.iguangwai;
 
 /**
- * Ӧ�ó��������ࣺ���ڱ��������Ϣ������
+ * 应用程序配置类：用于保存相关信息及设置
  */
 public class AppConfig {
 
 	public static String USERNAME;
-	public static final String loginUrl = "http://2.igdufs1.sinaapp.com/index.php/Home/Appsystemloadjson/jsontest";
+	public static final String loginUrl = "http://192.168.203.90/igdufs/index.php/home/AndroidApi/login";
 	public static final String articleUrl = "http://2.igdufs1.sinaapp.com/index.php/Home/Appstudypostsjson/postsjson";
 	public static final String contentUrl = "http://2.igdufs1.sinaapp.com/index.php/Home/Appsentpostjson/postcontentjson";
 	public static final String publishUrl = "http://2.igdufs1.sinaapp.com/index.php/Home/Appsentpostjson/sentpostjson";
